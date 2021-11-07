f721ad09df5b1031cfb3feff7eaf21ef12eb0524
Samuel Felipe <samuelfelipef@gmail.com>
2021-11-07 16:39:17 -0300

    update port on server.js console

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app.js';
 app.listen(4000, () => {
     console.clear();
     console.log(`Env: ${process.env.NODE_ENV}`);
-    console.log('server is running on port 4000');
+    console.log(`server is running on port ${process.env.PORT}`);
 });
