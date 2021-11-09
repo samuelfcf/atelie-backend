537314b05c3c798f902ae999190ecb3160030517
Samuel Felipe <samuelfelipef@gmail.com>
2021-11-09 14:46:57 -0300

    create products route and controller

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
+import getProducts from './controllers/products.js';
 import postUser from './controllers/users.js';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.status(200).send({
-    message: 'teste: welcome to atelie api!!',
+    message: 'test: welcome to atelie api!!',
   });
 });
 
 router.post('/sign-up', postUser);
+
+router.get('/products', getProducts);
 
 export default router;
