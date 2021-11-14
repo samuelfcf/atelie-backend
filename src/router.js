81145bf11de386044e01a07bfc01ade60a714554
okitauehara <marcosokita@live.com>
2021-11-14 17:13:44 -0300

    Checkout routes implemented

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { postUser, signInUser, signOutUser } from './controllers/users.js';
 import updateAddress from './controllers/addresses.js';
 import ensureAuth from './middlewares/ensureAnth.js';
+import { getFinalOrder, getOrderDetails } from './controllers/checkout.js';
 // eslint-disable-next-line object-curly-newline
 import { getProduct, getProducts, updateSizeQuantity, createCurrentOrder, createCart } from './controllers/products.js';
 
@@ -23,5 +24,8 @@ router.get('/product/:id', getProduct);
 router.post('/product/:id', ensureAuth, createCurrentOrder);
 router.post('/cart/:id', createCart);
 router.put('/product/:id', updateSizeQuantity);
+
+router.get('/cart-products/:orderId', ensureAuth, getFinalOrder);
+router.get('/checkout/:orderId', ensureAuth, getOrderDetails);
 
 export default router;
